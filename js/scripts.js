876d19c1c87087b35f33b768dd5f0971e3322a40
Valentinsan0107 <43492258+Valentinsan0107@users.noreply.github.com>
2022-10-13 06:47:15 +0200

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,27 @@
-// // Backend UI
-
 var user = {
   userName: "user",
   userFemale: true,
+  userJeunnon: true,
+  userMedicnon: true,
   userWeight: 0,
   userBac: 0,
   userTime: 0,
+  userAge : 0,
   userShot: 0,
   userChampagne: 0,
   userVin: 0,
   userPinte: 0,
+  userDegVin: 0,
+  userDegChampagne: 0,
+  userDegShot: 0,
+  userDegBiere: 0,
 };
+var updateUser_2 = function() {
+  if (document.getElementById("radioJeunoui").checked = true) {
+    user.userJeunnon = false;
+  };
+};
+
 var updateUser = function() {
   if (document.getElementById("radioMale").checked = true) {
     user.userFemale = false;
@@ -18,16 +29,13 @@ var updateUser = function() {
 };
 
 var updateBac = function() {
-  if (user.userFemale) {
-    user.userBac = ((0.118358 * (0.993822**(user.userWeight*2.2046))) * (user.userShot*1 + user.userChampagne*1 + user.userVin*1 + user.userPinte*3.6));
+  if (user.userFemale = true) {
+    user.userBac = ((0.118358 * (0.993822**(user.userWeight*2.2046))) * (user.userShot*1*(0.8*30*user.userDegShot/1000) + user.userChampagne*1*(0.8*100*user.userDegChampagne/1000) + user.userVin*1*(0.8*100*user.userDegVin/1000) + user.userPinte*1*(0.8*500*user.userDegBiere/1000)));
   } else {
-    user.userBac = ((0.115943 * (0.993822**(user.userWeight*2.2046))) * (user.userShot*1 + user.userChampagne*1 + user.userVin*1 + user.userPinte*3.6));
+    user.userBac = ((0.115943 * (0.993084**(user.userWeight*2.2046))) * (user.userShot*1*(0.8*30*user.userDegShot/1000) + user.userChampagne*1*(0.8*100*user.userDegChampagne/1000) + user.userVin*1*(0.8*100*user.userDegVin/1000) + user.userPinte*1*(0.8*500*user.userDegBiere/1000)));
   };
     user.userBac = (user.userBac - ((user.userTime / 40 * 0.01)));
 };
-
-
-// This function checks if you are under the legal limit to driveCheck
 
 var driveCheck = function() {
   if (user.userBac <= 0.7) {
@@ -38,8 +46,6 @@ var driveCheck = function() {
   };
 };
 
-// // Front End UI
-
 $(document).ready(function() {
   $("button#sub-button").click(function(event) {
     event.preventDefault();
@@ -48,9 +54,14 @@ $(document).ready(function() {
       user.userWeight = $("input#userWeight").val(),
       user.userTime = $("input#userTime").val(),
       user.userShot = $("input#qty").val(),
+      user.userAge = $("input#userAge").val(),
       user.userChampagne = $("input#qty_2").val(),
       user.userVin = $("input#qty_3").val(),
       user.userPinte = $("input#qty_4").val(),
+      user.userDegVin = $("input#userDegVin").val(),
+      user.userDegShot = $("input#userDegShot").val(),
+      user.userDegBiere = $("input#userDegBiere").val(),
+      user.userDegChampagne = $("input#userDegChampagne").val(),
       updateUser();
       updateBac();
       driveCheck();
